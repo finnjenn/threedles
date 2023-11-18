45e2cd1cb30e55bab77299f55987188eb2c835fc
finnjenn <finnjennn@gmail.com>
2023-11-18 15:51:41 -0500

    fixed file paths

--- shop.js
+++ shop.js
@@ -1,13 +1,11 @@
 // document.addEventListener("DOMContentLoaded", () => {
 // Get all product elements
 const productElements = document.querySelectorAll(".product-container");
-console.log(productElements);
 
 // Add event listeners to each product for hover effect
 productElements.forEach((product) => {
   let productImage = product.querySelector(".product-img");
   let altImage = product.querySelector(".product-alt-img");
-  console.log(productImage);
 
   // Add event listener for mouseover
   product.addEventListener("mouseenter", () => {
